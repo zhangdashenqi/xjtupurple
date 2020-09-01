f4a58b8dde7df641dff1a221afdf093986848961
endaytrer <endaytrer@gmail.com>
2020-09-01 12:55:52 +0800

    adjustable photo border width

--- js/script.js
+++ js/script.js
@@ -1,39 +1,27 @@
-$("document").ready(function () {
-    var qrCode = new QRCode("qrCode", {
+let codeFace;
+let refresh = () => {
+    $(".floatMenu").toggle("fast");
+    codeFace.clear();
+    document.getElementById("qrCodeDisplay").innerHTML = "";
+    if (app.qrCode === "") {
+        app.qrCode = app.generateCode();
+    }
+    codeFace = new QRCode("qrCodeDisplay", {
         text: "               " + app.qrCode + "               ",
         width: 150,
         height: 150,
         colorDark: app.colorOfCode,
         correctLevel: QRCode.CorrectLevel.H
     });
-    $("#menuToggler").click(function () {
-        $(".floatMenu").toggle("fast");
-        qrCode.clear();
-        document.getElementById("qrCode").innerHTML = "";
-        if (app.qrCode === "") {
-            app.qrCode = app.generateCode();
-        }
-        qrCode = new QRCode("qrCode", {
-            text: "               " + app.qrCode + "               ",
-            width: 150,
-            height: 150,
-            colorDark: app.colorOfCode,
-            correctLevel: QRCode.CorrectLevel.H
-        });
+}
+$("document").ready(() => {
+    codeFace = new QRCode("qrCodeDisplay", {
+        text: "               " + app.qrCode + "               ",
+        width: 150,
+        height: 150,
+        colorDark: app.colorOfCode,
+        correctLevel: QRCode.CorrectLevel.H
     });
-    $("#submitButton").click(function () {
-        $(".floatMenu").toggle("fast");
-        qrCode.clear();
-        document.getElementById("qrCode").innerHTML = "";
-        if (app.qrCode === "") {
-            app.qrCode = app.generateCode();
-        }
-        qrCode = new QRCode("qrCode", {
-            text: "               " + app.qrCode + "               ",
-            width: 150,
-            height: 150,
-            colorDark: app.colorOfCode,
-            correctLevel: QRCode.CorrectLevel.H
-        });
-    });
+    document.querySelector("#menuToggler").addEventListener("click", refresh);
+    document.querySelector("#submitButton").addEventListener("click", refresh);
 });
